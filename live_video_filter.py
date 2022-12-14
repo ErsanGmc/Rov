5455d721aa2e81db7c14b2f5878c25ea9338bed9
Ersan Gemici <ersangemici@protonmail.com>
2022-12-14 21:57:42 +0300

    renk filter red'e göre ayarladı.3 örnek konuldu

--- live_video_filter.py
+++ live_video_filter.py
@@ -4,41 +4,38 @@ import numpy as np
 camera = cv2.VideoCapture(0)
 def nothing(x):
     pass
-cv2.namedWindow("Deneme13")
-cv2.createTrackbar("H1","frame",0,359,nothing)
-cv2.createTrackbar("H2","frame",0,359,nothing)
+cv2.namedWindow("frame")
+cv2.createTrackbar("H1","frame",0,180,nothing)
+cv2.createTrackbar("H2","frame",0,180,nothing)
 cv2.createTrackbar("S1","frame",0,255,nothing)
 cv2.createTrackbar("S2","frame",0,255,nothing)
-cv2.createTrackbar("V1","frame",0,252,nothing)
-cv2.createTrackbar("V2","frame",0,252,nothing)
+cv2.createTrackbar("V1","frame",0,255,nothing)
+cv2.createTrackbar("V2","frame",0,255,nothing)
 
 while camera.isOpened():
     ret,frame =camera.read()
-    cv2.imshow("Real",frame)
-    # Ekran boyutlarını ayarlıyor
-    frame=cv2.resize(frame,(540,400),fx=0,fy=0,interpolation=cv2.INTER_CUBIC)
     
+    
+    cv2.imshow("Real",frame)
+    # Ekran boyutlarını ayarlıyor    
     HSV=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     
-    H1=cv2.getTrackbarPos("H1","FRAME")
-    H2=cv2.getTrackbarPos("H2","FRAME")
-    S1=cv2.getTrackbarPos("S1","FRAME")
-    S2=cv2.getTrackbarPos("S2","FRAME")
-    H3=cv2.getTrackbarPos("V1","FRAME")   
-    H3=cv2.getTrackbarPos("VE","FRAME")
+    H1=cv2.getTrackbarPos("H1","frame")
+    H2=cv2.getTrackbarPos("H2","frame")
+    S1=cv2.getTrackbarPos("S1","frame")
+    S2=cv2.getTrackbarPos("S2","frame")
+    V1=cv2.getTrackbarPos("V1","frame")   
+    V2=cv2.getTrackbarPos("V2","frame")
 
-    lower=np.array([H1,S1,H1])
-    upper=np.array([H2,S2,H3])
+    lower=np.array([H1,S1,V1])
+    upper=np.array([H2,S2,V2])
     
     mask=cv2.inRange(HSV,lower,upper)
     cv2.imshow("Video_without_Mask",HSV)
     masked_image=cv2.bitwise_and(frame,frame,mask=mask)
     cv2.imshow("Video_with_Mask",masked_image)
     
-    if not ret:
-     # Kameradan görüntü alınıp alınmadığını kontrol eder
-        print("Kameradan görütü alinamadi")
-        break
+
     # Kameradan görüntü alınırsa
     if cv2.waitKey(1) & 0xFF == ord("q"):
         print("Kamera sonlandirildi")
